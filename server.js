0d885464ac2d8ea9b91fa59debf8d08b47c68829
rpm3 <mancuso.b3@gmail.com>
2023-11-13 16:52:20 -0600

    Edited server.js and dependencies

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const path = require("path");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
-const routes = require('./controller');
-const helpers = require('./utils/helpers');
+const routes = require('./controllers');
+// const helpers = require('./utils/helpers');
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
